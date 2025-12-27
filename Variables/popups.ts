c8ea72cd194da5e9d20e973498803feca19ddb65
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-12-27 15:19:57 +0530

    popups

---
+++ Variables/popups.ts
@@ -0,0 +1,5 @@
+console.log("This is a popup message from popups.ts file.");
+alert("Hello");
+confirm("Are you sure you want to proceed?");
+var data: string | null = prompt("Please enter your name:", "Guest");
+console.log("User input from prompt: " + data);
