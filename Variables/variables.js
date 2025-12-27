608664992098caecaef417cb607723126b79cc0b
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-12-27 09:18:11 +0530

    variables any

--- Variables/variables.js
+++ Variables/variables.js
@@ -1,8 +1,8 @@
 // Declaring a variable with type annotation
-// This variable 'n1' is of type number
+// Declaring a number variable
 var n1 = 10;
 console.log(n1);
-// This is how to declare string variables using different quotation styles
+// Declaring string variables with different quote styles
 var s1 = "Hello, Typescript! Double quotes";
 var s2 = 'Hello, Typescript! Single quotes';
 var s3 = "Hello, Typescript! Backticks";
@@ -14,3 +14,12 @@ var b1 = true;
 var b2 = false;
 console.log(b1);
 console.log(b2);
+// Declaring any type variable
+var a1 = 42;
+console.log(a1);
+a1 = "Now I'm a string";
+console.log(a1);
+a1 = false;
+console.log(a1);
+a1 = { name: "Raviprasad", age: 25, city: "Bangalore", isStudent: true, hobbies: ["Reading", "Coding", "Travelling"], address: { street: "Street 123", zipcode: 560001 } };
+console.log(a1);
