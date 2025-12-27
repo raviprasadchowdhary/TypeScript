41672589d74932dd4d4a667ed2fdb6208c069291
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-12-27 09:24:38 +0530

    variables array

--- Variables/variables.js
+++ Variables/variables.js
@@ -23,3 +23,12 @@ a1 = false;
 console.log(a1);
 a1 = { name: "Raviprasad", age: 25, city: "Bangalore", isStudent: true, hobbies: ["Reading", "Coding", "Travelling"], address: { street: "Street 123", zipcode: 560001 } };
 console.log(a1);
+// Declaring array variables with different syntaxes
+var array1 = ["HP", "Dell", "Lenovo"];
+var array2 = ["Apple", "Samsung", "OnePlus"];
+console.log(array1);
+console.log(array2);
+console.log(array1[0]);
+console.log(array2[1]);
+console.log("Length of array1: " + array1.length);
+console.log("Length of array2: " + array2.length);
--- Variables/variables.ts
+++ Variables/variables.ts
@@ -29,3 +29,16 @@ a1 = false;
 console.log(a1);
 a1 = {name: "Raviprasad", age: 25, city: "Bangalore", isStudent: true, hobbies: ["Reading", "Coding", "Travelling"], address: {street: "Street 123", zipcode: 560001}};
 console.log(a1);
+
+// Declaring array variables with different syntaxes
+var array1: Array<String> = ["HP", "Dell", "Lenovo"];
+var array2: string[] = ["Apple", "Samsung", "OnePlus"];
+
+console.log(array1);
+console.log(array2);
+
+console.log(array1[0]);
+console.log(array2[1]);
+
+console.log("Length of array1: " + array1.length);
+console.log("Length of array2: " + array2.length);
