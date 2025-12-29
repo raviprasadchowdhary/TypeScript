9dc8fe8ac87921eb7ea8b2fdb3987a73fecc9651
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-12-29 21:17:21 +0530

    variables

---
+++ Variables/stringTypes.js
@@ -0,0 +1,4 @@
+var str1 = "Hello, World!";
+var username = "Raviprasad";
+var str2 = "Welcome ".concat(username, " to TypeScript");
+console.log(str2);
---
+++ Variables/stringTypes.ts
@@ -0,0 +1,7 @@
+var str1: String = "Hello, World!";
+
+var username: String = "Raviprasad";
+var str2: String = `Welcome ${username} to TypeScript`;
+
+console.log(str2);
+
